a62bc2260f3228a27fb65cee17426a6c2dd01935
Ben Border <ben@bunnbuns.net>
2021-04-27 15:09:16 -0400

    add hdata module and first api res

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,49 @@
+const HData = require('hdata').HData;
 const express = require('express')
 const app = express()
 var path = require('path')
 const port = 3000
+var conn;
+
+function connectTo(host, port) {
+	if(host == '') { host = '127.0.0.1'; }
+	if(port == '') { port = '8888'; }
+	console.log(`Connecting to HData Server at ${host} on port ${port}...`);
+	var options = {
+		"host": host,
+		"port": port
+	}
+	conn = new HData(options);
+  HDstatus();
+}
+
+function HDstatus() {
+	conn.status( (res, err) => {
+		if (!err) {
+      var response = `\n\rHData Server has the status: ${res.status}, and has ${res.jobs} pending jobs. ${res.tables} tables exist in the database.`;
+			console.log(response);
+		} else {
+			console.log(err);
+		}
+	})
+}
+
+app.get('/api/hdata/status', (req, res) => {
+  var response = "Not available";
+  conn.status((data, err) => {
+		if (!err) {
+      response = `\n\rHData Server has the status: ${data.status}, and has ${data.jobs} pending jobs. ${data.tables} tables exist in the database.`;
+		} else {
+			console.log(err);
+      data = err;
+		}
+    res.json(data);
+	})
+})
 
 app.use('/', express.static(path.join(__dirname, '/../static')))
 
 app.listen(port, () => {
   console.log(`HDataMyAdmin listening at http://localhost:${port}`)
+  connectTo();
 })
