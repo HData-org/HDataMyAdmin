49240627e645513ba1bd35cf876ae5225f83e715
Ben Border <ben@bunnbuns.net>
2021-04-27 16:19:18 -0400

    add color to bad status icon

--- src/static/assets/js/home.js
+++ src/static/assets/js/home.js
@@ -1,5 +1,5 @@
 function updateServerInfo(data) {
-    var html = '<span class="material-icons icon">link_off</span> <span>&nbsp;Server not connected</span>';
+    var html = '<span class="material-icons icon txt-red">link_off</span> <span>&nbsp;Server not connected</span>';
     if(data.status == 'OK') {
         html = '<span class="material-icons icon">link</span> <span>&nbsp;Server:&nbsp;</span> <span>localhost:8888</span>';
     }
