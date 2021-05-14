1684e315d1355fff84403f60a460cbdbfb2d64ee
Ben Border <ben@bunnbuns.net>
2021-05-14 16:22:46 -0400

    create table of tables

--- src/static/assets/js/tables.js
+++ src/static/assets/js/tables.js
@@ -1,5 +1,15 @@
-fetch("/api/hdata/gettables")
-.then(response => response.json())
-.then((data) => {
-    $('tables').textContent = JSON.stringify(data.value);
+fetch("/api/hdata/gettables").then(response => response.json()).then((data) => {
+    console.log(data);
+    var tables = document.createElement('table');
+    var tableHeader = document.createElement('th');
+    tableHeader.appendChild(document.createTextNode('Tables'));
+    tables.appendChild(tableHeader);
+    for(var i = 0; i <= data.value.length-1; i++) {
+        var row = document.createElement('tr');
+        var cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(data.value[i]));
+        row.appendChild(cell);
+        tables.appendChild(row);
+    }
+    $("tables").appendChild(tables);
 })
