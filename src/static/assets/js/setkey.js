7f39960992610f950e418cd2249c8c1d583117f3
Ben Border <ben@bunnbuns.net>
2021-05-21 15:52:13 -0400

    fix set key form

--- src/static/assets/js/setkey.js
+++ src/static/assets/js/setkey.js
@@ -5,3 +5,12 @@ updateNavTabs(page);
 document.title = tableName + " - Set Key - HDataMyAdmin";
 
 $('tableName').value = tableName;
+
+$("setKeyForm").onsubmit = (e) => {
+    var form = $("setKeyForm");
+    e.preventDefault();
+    let tableName = form.elements['tableName'].value;
+    let keyName = form.elements['keyName'].value;
+    let value = form.elements['value'].value;
+    setKey(tableName, keyName, value, "./table.html?name="+tableName);
+}
