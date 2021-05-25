a3bd78c6010e6b644f2e348889371b9d41b2d278
Ben Border <ben@bunnbuns.net>
2021-05-25 16:28:20 -0400

    clean up 2.0 lol

--- src/static/assets/js/login.js
+++ src/static/assets/js/login.js
@@ -15,6 +15,4 @@ function updateServerInfo(data) {
     }
 }
 
-fetch("/api/hdata/status")
-    .then(response => response.json())
-    .then(data => updateServerInfo(data));
+fetch("/api/hdata/status").then(response => response.json()).then(data => updateServerInfo(data));
