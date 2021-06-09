2afbb5fe23a54c049edc9a3809968bb96183f917
Ben Border <ben@bunnbuns.net>
2021-06-09 15:49:42 -0400

    start on new table view

--- src/static/assets/js/viewtable.js
+++ src/static/assets/js/viewtable.js
@@ -19,12 +19,66 @@ var breadcrumbsInfo = {
 }
 updateBreadcrumbs(breadcrumbsInfo);
 
-fetch("/api/hdata/tablekeys?tableName=" + tableName).then(response => response.json()).then((data) => {
+function showTable(tableData) {
+    var table = document.createElement("table");
+    var tableHeader = document.createElement("tr");
+    var tableHeaderRow = document.createElement("th");
+    tableHeaderRow.appendChild(document.createTextNode("Key"));
+    tableHeader.appendChild(tableHeaderRow);
+    table.appendChild(tableHeader);
+    tableHeaderRow = document.createElement("th");
+    tableHeaderRow.appendChild(document.createTextNode("Value"));
+    tableHeader.appendChild(tableHeaderRow);
+    table.appendChild(tableHeader);
+    for (var i = 0; i < tableData.length; i++) {
+        var rowData = tableData[i];
+        var row = document.createElement("tr");
+        var cell = document.createElement("td");
+        cell = document.createElement("td");
+        cell.appendChild(document.createTextNode(rowData.key));
+        row.appendChild(cell);
+        cell = document.createElement("td");
+        if (rowData.value === undefined) {
+            cell.setAttribute("class", "txt-red");
+        }
+        cell.appendChild(document.createTextNode(rowData.value));
+        row.appendChild(cell);
+        table.appendChild(row);
+    }
+    row = document.createElement("tr");
+    row.setAttribute("class", "tb-footer");
+    cell = document.createElement("td");
+    cell.appendChild(document.createTextNode(i + " Key(s)"));
+    row.appendChild(cell);
+    cell = document.createElement("td");
+    cell.setAttribute("class", "txt-right");
+    var link = document.createElement("a");
+    link.setAttribute("onclick", "exportJson(" + JSON.stringify(tableData) + ")");
+    link.appendChild(document.createTextNode("Show JSON"));
+    cell.appendChild(link);
+    row.appendChild(cell);
+    table.appendChild(row);
+    $("table").appendChild(table);
+}
+
+fetch("/api/hdata/querytable?evaluator=true&tableName=" + tableName).then(response => response.json()).then((data) => {
+    $("table").innerHTML = "";
     if (data.status !== "OK") {
         showErrorMsg("tableError", "Could not load table: " + errorCodeToMsg(data.status) + " (" + JSON.stringify(data) + ")");
+    } else {
+        showTable(data.matches);
     }
-    createTable1D($("table"), "Key", data.keys);
 }).catch((error) => {
     console.log(error);
     showErrorMsg("tableError", "Could not load users: " + error);
 });
+
+// fetch("/api/hdata/tablekeys?tableName=" + tableName).then(response => response.json()).then((data) => {
+//     if (data.status !== "OK") {
+//         showErrorMsg("tableError", "Could not load table: " + errorCodeToMsg(data.status) + " (" + JSON.stringify(data) + ")");
+//     }
+//     createTable1D($("table"), "Key", data.keys);
+// }).catch((error) => {
+//     console.log(error);
+//     showErrorMsg("tableError", "Could not load users: " + error);
+// });
